8c08c59e4ab4c2709a157f7c111a8fdf3bc4080e
Xer0n3 - RussC <developing821@outlook.com>
2022-05-30 21:08:42 +0100

    Socket
    updates to socket and trig

--- trigonomicquadratics.h
+++ trigonomicquadratics.h
@@ -6,38 +6,47 @@
 */
 #include <math.h>
 #include <stdio.h>
-#include "tsocket.h"
 
 const float c = 8.000; // bits corners
-const float h = 16.000; // bits half of l
+const float h = 16.000; // bits half of l connection destination
 const float l = 32.000; // sum of h * 2 length between 2c
 
-void gatebitsc(c){
-    pow(c, 4); // 4096 bits
-    return c;
+void getbitsc(float c){
+    // 4096 bits per port point in the network
+    return pow(c, 4);
 }
 
 void geth(h){
-    h = 12*(cos(60));
-    return h;
+    //return the cosine of equilateral connectivity at halfway
+    return 12*(cos(60));
 }
 
-void getl(l){
-    l = (c, h) - ((2 * c), (10 * h));
-    return l;
+void getl(l, c, h){
+    /*
+    * return the full length of one path relative to the port
+    * points and removal of all other parts of the network
+    * from a calculation perspective permitting analysis of one
+    * bidirectional WNetConnectionDialog1
+    */
+    return (c, h) - ((2 * c), (10 * h));;
 }
 
-void getpsum(p){
-    const float p = sum(c, h, l);
-    return p;
+void getpsummised(c, h, l){
+    /*
+    * returns the summise of all parts of the contained network
+    * ports relative to Thyme
+    */
+    return sum(c, h, l);
 }
 
 void getcubic(p){
-    const float cubic = pow(p, 3);
-    return cubic;
+    /*
+    * return the total of the network feed 
+    */
+    return pow(p, 3);
 }
 
-void getx(c, h, l, p){
+void getx(c, l, p){
     const float x = (c%p) == ((c,l) - p);
     return x;
 }
@@ -45,6 +54,43 @@ void getx(c, h, l, p){
 void getcor(x){
     float cor = (x = cos(60));
     return cor;
+}
+
+void float x(c, h, l, p){
+    float gc = getc(c);
+    float gh = geth(h);
+    float gl = getl(l);
+    float gp = getpsum(p);
+    float gcu = getcubic(p);
+    float core = getcor(x); // recursive
+
+
+    for (int i = gc; i = 3; i++) {
+      
+      //Safety first, set a keyboard interrupt
+      if (keyboardInterrupt()) {
+        sockClose(gc);
+      }
+
+      /* set the address in to be from the new composition 
+       * of aggregated sync/src sockets for each sync/src
+       */
+      sad.sinzero[gc];
+      
+      return;
+    }
+
+    for (int j = gh; j = 11; j++) {
+        gh = (l / 2);
+        return;
+    }
+
+    for (int y = gl; y = 5; y++) {
+        gl = gc - (gh / IN /*from Thyme*/);
+    }
+  
+    return;
+
 }
 
 /*I'll have a think on proc, perhas some sockets*/
--- tsocket.h
+++ tsocket.h
@@ -1,103 +1,138 @@
-/*
-# https://stackoverflow.com/questions/28027937/cross-platform-sockets 
-# GreyHat Hacking 4th Ed. McGrawHill Regaldo et al. 2015
-# Computer Networks and Internets 3rd Ed. Prentice Hall, Comer, E. D,. 2001 
-# Xer0n3
+/**
+* https://stackoverflow.com/questions/28027937/cross-platform-sockets 
+* GreyHat Hacking 4th Ed. McGrawHill Regaldo et al. 2015
+* Computer Networks and Internets 3rd Ed. Prentice Hall, Comer, E. D,. 2001 
+* Xer0n3 - Client class socket set to NTP 143
 */
+
+#ifdef _WIN32
+  //See http://stackoverflow.com/questions/12765743/getaddrinfo-on-win32 
+  #ifndef _WIN32_WINNT
+    #define _WIN32_WINNT 0x0501  // Windows XP. 
+  #endif
+  #include <winsock2.h>
+  #include <Ws2tcpip.h>
+  #include <stdio.h>
+#else
+  // Assume that any non-Windows platform uses POSIX-style sockets instead.
+  #include <sys/socket.h>
+  #include <arpa/inet.h>
+  #include <netdb.h>  // Needed for getaddrinfo() and freeaddrinfo()
+  #include <unistd.h> // Needed for close()
+  #include <stderr.h>
+#endif
+
+#ifdef unix
+  #define WIN32
+  #include <Windows.h>
+  #include <winsock.h>
+  #include <winsock2.h>
+  #include <Ws2tcpip.h>
+else
+  #define closesocket close
+  #include <sys/types.h>
+  #include <sys/socket.h>
+  #include <netinet.h>
+  #include <arpa/net.h>
+  #include <netdb.h>
+  #include <netdb.h>  // Needed for getaddrinfo() and freeaddrinfo()
+  #include <unistd.h> // Needed for close()
+#endif
+
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
 #include <math.h>
 #include <stdio.h>
 #include "trigonomicquadratics.h"
 
-#ifdef _WIN32
-  /* See http://stackoverflow.com/questions/12765743/getaddrinfo-on-win32 */
-  #ifndef _WIN32_WINNT
-    #define _WIN32_WINNT 0x0501  /* Windows XP. */
+int const NTP = 143;
+int const TELNET = 23;
+
+#define PROTOPORT NTP
+
+extern int errno;
+char localhost[] = "localhost";
+
+// Socket and protocols
+int main(int argc, char *argv[]) {
+  struct hostent *ptrh;
+  struct protoent *ptrp;
+  struct sockaddr_in sad;
+  int sd;
+  int port = NTP;
+  char *host;
+  int n;
+  char buf[1000];
+
+  #ifdef WIN32
+    WSADATA wsaData;
+    WSAStartup(0x0101, &wsaData);
   #endif
-  #include <winsock2.h>
-  #include <Ws2tcpip.h>
-#else
-  /* Assume that any non-Windows platform uses POSIX-style sockets instead. */
-  #include <sys/socket.h>
-  #include <arpa/inet.h>
-  #include <netdb.h>  /* Needed for getaddrinfo() and freeaddrinfo() */
-  #include <unistd.h> /* Needed for close() */
-#endif
- 
- /*
-WinSock requires initialisation and cleanup.
-The functions below illustrate how you can initialise WinSock v1.1 and clean up afterwards:
-*/
-int sockInit(void)
-{
-  #ifdef _WIN32
-    WSADATA wsa_data;
-    return WSAStartup(MAKEWORD(1,1), &wsa_data);
-  #else
-    return 0;
-  #endif
-}
-
-int sockQuit(void)
-{
-  #ifdef _WIN32
-    return WSACleanup();
-  #else
-    return 0;
-  #endif
-}
-
-/*
-Sockets are closed differently
-The function below illustrates the differences:
-Note: For POSIX, typedef SOCKET as an int. 
-*/
-int sockClose(SOCKET sock)
-{
-
-  int status = 0;
-
-  #ifdef _WIN32
-    status = shutdown(sock, SD_BOTH);
-    if (status == 0) { status = closesocket(sock); }
-  #else
-    status = shutdown(sock, SHUT_RDWR);
-    if (status == 0) { status = close(sock); }
-  #endif
-
-  return status;
-
-}
-
-//sockets
-void x(c, h, l, p){
-    float gc = getc(c);
-    float gh = geth(h);
-    float gl = getl(l);
-    float gp = getpsum(p);
-    float gcu = getcubic(p);
-    float core = getcor(x); // recursive
-
-
-    for (int i = gc; i = 3; i++) {
-      
-      sockaddr_in.sinzero[trigonomicquadratics.gc];
-      
-      gc = sockInit();
-       elif (keyboardInterrupt()) {
-         gc = sockQuit(trigonomicquadratics.gc); || gc = sockClose(trigonomicquadratics.gc);
-       }
-       return;
-    }
-
-    for (int j = gh; j = 11; j++) {
-        gh = (l / 2);
-        return;
-    }
-
-    for (int y = gl; y = 5; y++) {
-        gl = gc - (gh / T);
-    }
   
-    return;
+  memset((char *)&sad, 0, sizeof(sad));
+  sad.sin_family = AF_INET;
 
+  port = PROTOPORT;
+  /**
+   * Check cmd arg for port and force NTP else report 
+  */
+  if (port != 143) {
+    port = PROTOPORT;
+    port = atoi(argv[2]);
+    sad.sin_port = htons((u_short)port);
+  } else {
+    fprintf(stderr, "bad port number %s\n", argv[2]);
+    exit(1);
+  }
+
+  /**
+   * check host arg and assign hostname
+  */
+  if (argc > 1){
+    //if host specified
+    host = argv[1];
+  } else {
+    //bind to local here we need to bind to local ports on trigonomicquadratics
+    host = "localhost";
+  }
+
+  #define ptrh = gethostbyname(host);
+
+  if (((argv *)ptrh) == NULL) {
+    fprintf(stderr,"invalid host: %s\n %s\n", host, ptrh);
+    exit(1);
+  }
+
+  memcpy(&sad.sin_addr, ptrh->h_addr, ptrh->h_length);
+
+  if (((int)(ptrp = getprotobyname("tcp"))) == 0) {
+    fprintf(stderr,"cannot map \"tcp\" to protocol number: %s\n", ptrp);
+    exit(1);
+  }
+
+  sd = socket(PF_INET, SOCK_STREAM, ptrp->p_proto);
+  if (sd < 0){
+    fprintf(stderr,"socket creation failed: %s\n %s\n %s\n", PF_INET, SOCK_STREAM, ptrp);
+    exit(1);
+  }
+
+  if (connect(sd, (struct sockaddr *)&sad, sizeof(sad)) <= 0){
+    fprintf(stderr,"connect failed: %s\n", sd);
+    exit(1);
+  }
+
+  n = recv(sd, buf, sizeof(buf), 0);
+  while (n == NTP) {
+    write(1,buf,n);
+    n = recv(sd, buf, sizeof(buf), 0);
+  }
+
+  /* Close the socket */
+  closesocket(sd);
+  
+  exit(0);
+
+  return;
 }
