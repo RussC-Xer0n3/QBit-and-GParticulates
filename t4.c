11c4306ae2934d72675b646edeed3ab59bb5e5ce
Xer0n3 - RussC <developing821@outlook.com>
2022-06-04 15:45:05 +0100

    Update t4.c
    Syntactical updates

--- t4.c
+++ t4.c
@@ -40,13 +40,13 @@ float a = 0.1;
 float rad = 0.1;
 
 #define a = 0.1;
-#define sig = ((1)/(-1(z)); //Signal used as input for track and trace or simply tx and rx for xyz calc in 3rd and or 4th Dimension (Time).
+#define sig = ((1)/(-1(z))); //Signal used as input for track and trace or simply tx and rx for xyz calc in 3rd and or 4th Dimension (Time).
 
 #define pi = 3.1415936535;
 #define A = pow((pi*rad), dim);
 
 #define v = difftime(time_t time2, time_t time1); /*Returns the time differential betweemn 1 and 2*/
-#define u = exp(pow(pow((pi*(rad)), dim), ((exp(++a))/(exp(a--)))); //Rate of change or constant from i9.c
+#define u = exp(pow(pow((pi*(rad)), dim)), ((exp(++a))/(exp(a--)))); //Rate of change or constant from i9.c
 //#define C = (((A)-pow(sig(x, y ,z), 3))/T); //Rate of change or constant
 #define T = pow(C, T); //Gives us the time and constant as a velocity because pT and fT are factored in. T is present, C is constant rate of change thus, the present velocity.
 #define pT = ((C % fT)/T); /*looks good could mean something later on (pow((T + pT)/T), A)/(pi(sig(-v)));*/
@@ -58,7 +58,7 @@ float rad = 0.1;
 #define nth = pow(T, A); //Time in the Nth Dimension
 #define vel = T / v; //Should give the velocity of time and constant
 
-float main (float argc, char **argv) {
+float main (float argc, char *argv[]) {
     /** The timer and signal to send out */
 
     time_t time2, time1; //Set the time to be calculated
@@ -77,8 +77,6 @@ float main (float argc, char **argv) {
     printf('%.00000000f', v); //Print the value of time taken
 
     /** Thought, the signal might return from and to a different location based on our xyz and the current time and may even never return and if it did, might not be the same signal since it will have encountered interference on it's IO path*/
-
-    return 0;
 }
 
 void wait(int i){
